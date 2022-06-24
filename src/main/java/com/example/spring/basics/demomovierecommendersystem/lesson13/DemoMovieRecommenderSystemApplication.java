a849e0ee86a1d994e435f0b94e824357e969af8a
hks1 <hks1@users.noreply.github.com>
2022-06-24 13:47:59 -0400

    Spring Application Configuration, changes pom for lesson13

--- src/main/java/com/example/spring/basics/demomovierecommendersystem/lesson13/DemoMovieRecommenderSystemApplication.java
+++ src/main/java/com/example/spring/basics/demomovierecommendersystem/lesson13/DemoMovieRecommenderSystemApplication.java
@@ -2,27 +2,35 @@ package com.example.spring.basics.demomovierecommendersystem.lesson13;
 
 import java.util.Arrays;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.annotation.processing.SupportedAnnotationTypes;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /*
  * Autowiring - @Qualifier
  */
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@ComponentScan
 public class DemoMovieRecommenderSystemApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(DemoMovieRecommenderSystemApplication.class, args);
 	    
 	    // ApplicationContext manages the beans and dependencies
+	    /* Change this:
 	    ApplicationContext appContext = SpringApplication.run(DemoMovieRecommenderSystemApplication.class, args);
+	    */
+	    
+	    // to this:
+	    AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(DemoMovieRecommenderSystemApplication.class);
 
-	    //create object of RecommenderImplementation class
-	    // passing name of the filter as constructor argument
-        //RecommenderImplementation recommendor = new RecommenderImplementation(filter);
-	    //RecommenderImplementation recommendor = new RecommenderImplementation(new ContentBasedFilter());
 	    // use ApplicationContext to find which filter is being used
 	    RecommenderImplementation recommender = appContext.getBean(RecommenderImplementation.class); 
         
